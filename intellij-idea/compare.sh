755f80cdf7cb57ac3e8bc76db9a8087128acacf7
Basti Tee <basti.tee@gmx.de>
2017-08-04 11:44:16 +0200

    intellij settings;

--- intellij-idea/compare.sh
+++ intellij-idea/compare.sh
@@ -5,13 +5,15 @@ function print () {
 }
 
 ideapath=$( find ~ -maxdepth 1 -iname ".Idea*" | head -n1 )
+ideapath=${ideapath}/config
 echo "--- comparing with: $ideapath"
 cd "$( dirname "$( readlink -f "$0" )" )"
-find -type f ! -iname "*.sh" | while read file
+find -type f ! -iname "*.sh" | while read ofile
 do
-    bn=$( basename $file )
-    ifile=$( find $ideapath -iname "$bn" )
-    [ -z "$ifile" ] && { print "--- $bn not installed."; continue; }
-    print "--- $bn <<>> $ifile"
-    diff $bn $ifile
+    file=$( sed -e "s/_\///" <<< $ofile )
+    # bn=$( basename $file )
+    ifile=$( ls ${ideapath}/$file 2> /dev/null )
+    [ -z "$ifile" ] && { print "--- $file not installed."; continue; }
+    print "--- $file <<>> $ifile"
+    diff $ofile $ifile
 done
--- intellij-idea/link.sh
+++ intellij-idea/link.sh
@@ -1,8 +1,18 @@
 cd $( dirname $( readlink -f $0 ))
 ideapath=$( find ~ -maxdepth 1 -iname ".Idea*" | head -n1 )
-echo "--- installing to: $ideapath"
-../mklinks.sh colortheme-basti-linux.icls ${ideapath}/config/colors
-../mklinks.sh keymap-basti.xml ${ideapath}/config/keymaps
-../mklinks.sh codestyle-basti.xml ${ideapath}/config/codestyles
-../mklinks.sh inspection-basti.xml ${ideapath}/config/inspection
-../mklinks.sh "web-browsers.xml editor.xml ide.general.xml keyboard.xml ui.lnf.xml code.style.schemes.xml colors.scheme.xml keymap.xml" ${ideapath}/config/options
+echo -e "--- installing to: $ideapath\n"
+mkdir -vp ${ideapath}/config
+find ${ideapath}/config -type l | xargs rm -v 
+echo
+cd _
+for file in *; do
+    ../../mklinks.sh $file ${ideapath}/config
+echo; done
+cd ..
+
+for folder in codestyles colors inspection keymaps options; do
+    mkdir -vp ${ideapath}/config/$folder
+    for file in $folder/*; do
+        ../mklinks.sh $file ${ideapath}/config
+    echo; done
+done
