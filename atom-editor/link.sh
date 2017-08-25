92207a0542b344a0b2adc5d91badd8b8870650c7
Basti Tee <basti.tee@posteo.de>
2017-08-25 19:46:36 +0200

    added-hostspecifc-config

--- atom-editor/link.sh
+++ atom-editor/link.sh
@@ -1,2 +1,24 @@
+#!/bin/bash
 cd $( dirname $( readlink -f $0 ))
-../mklinks.sh "*.coffee *.cson *.less" ~/.atom
+
+function link() {
+    rm -vf "$2"
+    ln -vs "$(pwd)/$1" "$2"
+}
+
+target=~/.atom
+hn=$( hostname )
+mkdir -vp ${target}
+
+# defaults
+link keymap.cson ${target}/keymap.cson
+link snippets.cson ${target}/snippets.cson
+link init.coffee ${target}/init.coffee
+# machine-specific 
+[ $hn == "zenbook" ] && config="config.zenbook.cson" || config="config.cson"
+link $config ${target}/config.cson
+[ $hn == "zenbook" ] && config="styles.zenbook.less" || config="styles.less"
+link $config ${target}/styles.less
+
+#
+ls -l ~/.atom
