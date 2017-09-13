41e6ecfb78236ebe9178cec4580717780ec2a79c
Basti Tee <basti.tee@gmx.de>
2017-09-13 09:15:41 +0200

    nativefier;

--- nativefier/nativefier-setup.sh
+++ nativefier/nativefier-setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 cd "$( dirname "$( readlink -f "$0" )" )"
 [ -z $( command -v nativefier ) ] && sudo npm install -g nativefier
-defaults="--disable-dev-tools --disable-context-menu"
 [ $( hostname ) == "zenbook" ] && defaults="$defaults --zoom 1.5"
 
 #------------------------------------------------------------------------------
-# HELPER 
+# HELPER
 #------------------------------------------------------------------------------
 
 function desktop_link () {
-trg=~/.local/share/applications/nativefier_${1}.desktop
+    trg=~/.local/share/applications/nativefier_${1}.desktop
 cat << EOF > $trg
 [Desktop Entry]
 Encoding=UTF-8
@@ -20,24 +19,26 @@ Icon=$( dirname $( readlink -f $2 ) )/resources/app/icon.png
 Exec=$( readlink -f $2 )
 StartupWMClass=$1
 EOF
-chmod 755 $trg
-echo "----------"
-cat $trg
-echo "----------"
+    chmod 755 $trg
+    echo "----------"
+    cat $trg
+    echo "----------"
 }
 
 function run_nativefier () {
     url=$1
-    shift 
+    shift
     name=$1
     shift
     rm -rf build/$name-linux-x64
-    nativefier $url -n $name ${defaults} $@ build/
+    nativefier $url -n $name \
+    --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/60.0.3112.113 Chrome/60.0.3112.113 Safari/537.36" --disable-dev-tools --disable-context-menu \
+    $@ build/
     desktop_link $name build/$name-linux-x64/$name
 }
 
 #------------------------------------------------------------------------------
-# SETUPS 
+# SETUPS
 #------------------------------------------------------------------------------
 
 mkdir -vp build
@@ -48,10 +49,9 @@ then
     exit 0
 fi
 
-# otherwise run standards 
-run_nativefier https://trello.com/b/cEBcoTqe/scopevisio trello --maximize --inject trello.css
+# otherwise run standards
 
-run_nativefier https://wunderlist.com/de/\#/lists/inbox wunderlist 
+run_nativefier https://wunderlist.com/de/\#/lists/inbox wunderlist
 
 run_nativefier https://twitter.com/ twitter
 
