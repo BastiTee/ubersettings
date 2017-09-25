7ac9444aeec30f3fb0f7d200c6ef44446a304520
Basti Tee <basti.tee@gmx.de>
2017-09-25 09:36:54 +0200

    added gnome-shell convenience;

--- README.md
+++ README.md
@@ -4,15 +4,6 @@
 
 ## Notes
 
-### Gnome shell extensions
-
-- [Dash to Dock](https://extensions.gnome.org/extension/307/dash-to-dock/)
-- [TopIcon Plus](https://extensions.gnome.org/extension/1031/topicons/)
-- [Put Windows](https://extensions.gnome.org/extension/39/put-windows/)
-- [No Topleft Hot Corner](https://extensions.gnome.org/extension/118/no-topleft-hot-corner/)
-- [Disable Workspace Switcher Popup](https://extensions.gnome.org/extension/959/disable-workspace-switcher-popup/)
-
-
 ### Iconset
 
 - [Numix Circle](https://github.com/numixproject/numix-icon-theme-circle)
---
+++ ubuntu/gnome-shell/setup-extensions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd "$( dirname "$( readlink -f "$0" )" )"
+GEM="./gnomeshell-extension-manage"
+USER_PATH="$HOME/.local/share/gnome-shell/extensions"
+SYSTEM_PATH="/usr/share/gnome-shell/extensions"
+
+echo "REMOVE PREINSTALLED EXTENSIONS..."
+
+rem_system="sudo $GEM --remove --system --extension-id"
+$rem_system 6 # /applications-menu/
+$rem_system 16 # /auto-move-windows/
+$rem_system 600 # /launch-new-instance/
+$rem_system 18 # /native-window-placement/
+$rem_system 8 # /places-status-indicator/
+$rem_system 19 # /user-themes/
+$rem_system 602 # /window-list/
+$rem_system 10 # /windownavigator/
+$rem_system 7 # /removable-drive-menu/
+$rem_system 881 # /screenshot-window-sizer/
+$rem_system 21 # /workspace-indicator/
+$rem_system 15 # /alternatetab/
+
+echo "RE-INSTALL USER EXTENSIONS..."
+
+inst_user="$GEM --install --user --extension-id"
+$inst_user 15 # /alternatetab/
+$inst_user 307 # /dash-to-dock/
+$inst_user 959 # /disable-workspace-switcher-popup/
+$inst_user 118 # /no-topleft-hot-corner/
+$inst_user 39 # /put-windows/
+$inst_user 1031 # /topicons/
+
+echo "--- USER PATH:"
+ls -l $USER_PATH
+echo "--- SYS PATH:"
+ls -l $SYSTEM_PATH
