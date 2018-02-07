e809051fbc36245d052237d0c07113d313d7d530
Basti Tee <basti.tee@gmx.de>
2018-02-07 16:21:31 +0100

    better mc colors; better script to test mc colors;

--- shell-rcs/mc-colors-convert.sh
+++ shell-rcs/mc-colors-convert.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd "$( dirname "$( readlink -f "$0" )" )"
+tf=$( mktemp )
 {
-    echo base_color=linux:
-    cat mc-colors-test.sh | grep -v -e "#" -e "^mc " | sed -e "s/:.*$/:/g"
-} | tr -d "\n" 
-echo
+echo base_color=linux:
+cat mc-colors-test.sh | grep -v -e "#" -e "^mc " | sed -e "s/:.*$/:/g"
+} > $tf
+new_colors=$( cat $tf | tr -d "\n" )
+rm -f $tf
+sed -i "s/^base_color=linux:.*/$new_colors/" ini
+
