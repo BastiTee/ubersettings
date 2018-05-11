56930632507227071522f663cb27591febcf7ec4
Basti Tee <basti.tee@posteo.de>
2018-05-11 20:08:01 +0200

    zenbook intelllij

--- intellij-idea-mac/link.sh
+++ intellij-idea-mac/link.sh
@@ -1,17 +1,36 @@
+#!/bin/bash
+
 cd $( dirname $( realpath $0 ))
-ideapath=$( find ~/Library/Preferences -type d -maxdepth 1 -iname "Idea*" |\
-head -n1 )
+
+# Lookup configuration folder
+if [ "$( hostname )" == "zenbook" ]; then
+    ideapath=$( find ~ -maxdepth 1 -type d  \
+    -iname ".Idea*" | head -n1 )
+    ideapath=${ideapath}/config
+else
+    ideapath=$( find ~/Library/Preferences -type d -maxdepth 1 \
+    -iname "Idea*" | head -n1 )
+fi
 echo -e "--- installing to: $ideapath\n"
 
+# Link global exception configurations
 cd _
 for file in *; do
     ../../mklinks.sh $file ${ideapath}
 echo; done
 cd ..
 
-for folder in codestyles inspection keymaps options; do
+# Link host-independent settings
+for folder in codestyles inspection options; do
     mkdir -vp ${ideapath}/$folder
     for file in $folder/*; do
         ../mklinks.sh $file ${ideapath}/
     echo; done
 done
+
+# Link host-dependent settings
+mkdir -vp ${ideapath}/keymaps
+src="keymaps/keymap-basti.xml"
+[ "$( hostname )" == "zenbook" ] && src="keymaps/keymap-basti-zenbook.xml"
+rm -vf ${ideapath}/keymaps/keymap-basti.xml
+ln -vs $src ${ideapath}/keymaps/keymap-basti.xml
