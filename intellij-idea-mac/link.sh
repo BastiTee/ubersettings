70e1b3f03f2833e2038d053ce8668d1f15edea94
Basti Tee <basti.tee@posteo.de>
2018-05-11 09:25:56 +0200

    finished vc'ing mac intellij;

--- intellij-idea-mac/link.sh
+++ intellij-idea-mac/link.sh
@@ -3,16 +3,12 @@ ideapath=$( find ~/Library/Preferences -type d -maxdepth 1 -iname "Idea*" |\
 head -n1 )
 echo -e "--- installing to: $ideapath\n"
 
-# mkdir -vp ${ideapath}/config
-# find ${ideapath}/config -type l | xargs rm -v
-# echo
-# cd _
-# for file in *; do
-#     ../../mklinks.sh $file ${ideapath}/config
-# echo; done
-# cd ..
+cd _
+for file in *; do
+    ../../mklinks.sh $file ${ideapath}
+echo; done
+cd ..
 
- #keymaps options
 for folder in codestyles inspection keymaps options; do
     mkdir -vp ${ideapath}/$folder
     for file in $folder/*; do
