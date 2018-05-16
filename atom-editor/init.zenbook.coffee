cf73f56a69ee674e8e8321499248ee7880bea865
Basti Tee <basti.tee@posteo.de>
2018-05-16 14:22:02 +0200

    Atom on mac

---
+++ atom-editor/init.zenbook.coffee
@@ -0,0 +1,2 @@
+# Your init script
+
--- atom-editor/link.sh
+++ atom-editor/link.sh
@@ -2,7 +2,7 @@
 
 function link() {
     rm -vf "$2"
-    ln -vs "$(pwd)/$1" "$2"
+    ln -vs "$1" "$2"
 }
 
 here="$( dirname $( realpath $0 ))"
@@ -15,7 +15,7 @@ mkdir -vp ${target}
 link ${here}/keymap${rd}.cson ${target}/keymap.cson
 link ${here}/snippets.cson ${target}/snippets.cson
 link ${here}/config.cson ${target}/config.cson
-link ${here}/init.coffee ${target}/init.coffee
+link ${here}/init${rd}.coffee ${target}/init.coffee
 link ${here}/styles.less ${target}/styles.less
 
 ls -l ${target}
