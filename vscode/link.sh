f2536c4d053c5b524be0b357d7b8f646f8dc351c
Basti Tee <basti.tee@posteo.de>
2018-09-21 14:04:25 +0200

    Vscode settings

---
+++ vscode/link.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function link() {
+    rm -vf "$2"
+    ln -vs "$1" "$2"
+}
+
+here="$( cd "$( dirname "$0" )"; pwd )"
+target=~/Library/Application\ Support/Code/User/
+mkdir -vp "${target}"
+
+[ $( hostname ) == "zenbook" ] && rd=".zenbook" ||rd=""
+
+# defaults
+link "${here}/settings${rd}.json" "${target}/settings.json"
+
+ls -l "${target}"
