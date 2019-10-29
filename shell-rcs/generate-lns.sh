9ce599686302d85348346deecba22025cca8f1d4
Basti Tee <basti.tee@posteo.de>
2019-10-29 11:03:31 +0100

    ln -s

--- shell-rcs/generate-lns.sh
+++ shell-rcs/generate-lns.sh
@@ -3,6 +3,5 @@ cd "$( cd "$( dirname "$0" )"; pwd )"
 
 for f in `find . -type f`; do
     if [ "$f" = "." ]; then continue; fi
-    if [[ "$f" =~ .*\.sh ]]; then continue; fi
     echo "ln -s $( pwd )$( dirname $f | sed -e "s/^\.// " )/$( basename $f )"
 done
